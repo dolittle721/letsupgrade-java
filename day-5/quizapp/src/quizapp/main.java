001fa2d25af669039a0c5f932b87a6d625c55fba
dolittle721 <67226103+dolittle721@users.noreply.github.com>
2020-10-16 21:22:59 +0530

    Add files via upload

---
+++ day-5/quizapp/src/quizapp/main.java
@@ -0,0 +1,14 @@
+package quizapp;
+
+public class main {
+	public static void main(String[] args) {
+		//Quiz App
+		//game
+		//question
+		//player
+		Game game=new Game();
+        game.initGame();
+        game.play();
+	}
+
+}
